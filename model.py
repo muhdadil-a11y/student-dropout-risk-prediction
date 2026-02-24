a62ed2e7369f3a8215994201ff90359d3d230a46
muhdadil-a11y <muhdadil6404@gmail.com>
2026-02-25 02:56:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,31 +1,22 @@
-# =========================================
+
 # IMPORT LIBRARIES
-# =========================================
 import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 
 import warnings
 warnings.filterwarnings("ignore")
 
-
-# =========================================
 # LOAD DATASET
-# =========================================
 df = pd.read_csv("Dataset/student_risk_dataset_5000.csv")
-
 X = df.drop("Dropout_Risk", axis=1)
 y = df["Dropout_Risk"]
 
-
-# =========================================
 # SCALING & SPLIT
-# =========================================
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
@@ -33,10 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42, stratify=y
 )
 
-
-# =========================================
 # TRAIN MODEL (XGBOOST)
-# =========================================
 model = XGBClassifier(
     n_estimators=200,
     learning_rate=0.05,
@@ -50,10 +38,7 @@ model.fit(X_train, y_train)
 print("\nModel trained successfully!")
 print("Model Accuracy:", accuracy_score(y_test, model.predict(X_test)))
 
-
-# =========================================
 # TAKE USER INPUT
-# =========================================
 print("\n--- ENTER STUDENT DETAILS ---")
 
 attendance = float(input("Enter Attendance Percentage (0–100): "))
@@ -62,20 +47,14 @@ grade = float(input("Enter Average Grade (0–10): "))
 engagement = int(input("Enter Engagement Score (1–10): "))
 task_completion = float(input("Enter Task Completion Rate (0–100): "))
 
-
-# =========================================
 # PREDICTION
-# =========================================
 student_data = np.array([[attendance, units, grade, engagement, task_completion]])
 student_scaled = scaler.transform(student_data)
 
 prediction = model.predict(student_scaled)[0]
 probability = model.predict_proba(student_scaled)[0][1]
 
-
-# =========================================
 # OUTPUT
-# =========================================
 print("\n--- PREDICTION RESULT ---")
 
 if prediction == 1:
